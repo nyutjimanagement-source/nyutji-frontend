e45ff53fedbf28acd3bcbb91b4390fd42acf3c72
nyutjimanagement-source <nyutji.management@gmail.com>
2026-04-17 23:57:52 +0700

    Fix: Restore Kotlin to 2.3.10 to match stdlib dependency resolution

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -31,7 +31,7 @@ dependencyResolutionManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
 }
 
 include(":app")
