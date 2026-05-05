7511cddb4777868c29d4e9c85d9d47669c556f55
nyutjimanagement-source <nyutji.management@gmail.com>
2026-05-06 00:29:46 +0700

    fix: add missing foundation import for kDebugMode in payment screen

--- lib/features/customer_pl/screens/customer_payment_screen.dart
+++ lib/features/customer_pl/screens/customer_payment_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import 'package:intl/intl.dart';
