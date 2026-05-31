ef47f7a1d7506dbfb24b235e1d3badf4e31ca33b
nyutjimanagement-source <nyutji.management@gmail.com>
2026-06-01 00:56:35 +0700

    fix: remove unnecessary foundation import in mitra_mesin.dart

--- lib/features/mitra_ml/screens/mitra_mesin.dart
+++ lib/features/mitra_ml/screens/mitra_mesin.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import 'package:provider/provider.dart';
